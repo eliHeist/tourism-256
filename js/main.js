c95c46e4a8dd29b3dbd3e0729bb8ea37f27907c2
Elijah <eliyang256@outlook.com>
2024-02-10 18:05:05 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,4 @@
+upArea = document.querySelector('.nav-btn.top-right')
+downArea = document.querySelector('.nav-btn.bottom-right')
+leftArea = document.querySelector('.nav-btn.middle-left')
+rightArea = document.querySelector('.nav-btn.middle-right')
